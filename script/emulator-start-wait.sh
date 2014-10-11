cad23c926090633608aa574300880b82ec6f561c
Graham Holker <graham.holker@gmail.com>
2014-10-11 11:52:13 -0400

    Target 20 / android L

--- script/emulator-start-wait.sh
+++ script/emulator-start-wait.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-echo no | android create avd --force -n test -t android-19 --abi x86
+echo no | android create avd --force -n test -t android-l --abi x86
 emulator -avd test -no-skin -no-audio -no-window &
 ./script/android-wait-for-emulator.sh
 adb shell input keyevent 82 &
